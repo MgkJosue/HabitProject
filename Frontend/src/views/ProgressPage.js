4f75cbaac546ba036bb7a3356ae23d9d222577c9
Nelson Molina <geovannysangucho25@gmail.com>
2023-07-27 16:49:12 -0500

    Styles2

--- Frontend/src/views/ProgressPage.js
+++ Frontend/src/views/ProgressPage.js
@@ -13,9 +13,15 @@ import { makeStyles } from '@material-ui/core/styles';
 // Importando componentes de Recharts
 import { LineChart, Line, CartesianGrid, XAxis, YAxis } from 'recharts';
 
+// Importar la imagen local
+import backgroundImage from '../img/gif1.gif';
+
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
+    height: '100vh', // Ajustar el contenedor principal al 100% del alto de la pantalla
+    display: 'flex',
+    flexDirection: 'column', // Alinear los elementos en columna
   },
   menuButton: {
     marginRight: theme.spacing(2),
@@ -27,6 +33,26 @@ const useStyles = makeStyles((theme) => ({
     padding: theme.spacing(2),
     textAlign: 'center',
     color: theme.palette.text.secondary,
+  },
+  imageContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: '100%', // Agrega bordes redondeados al contenedor
+    overflow: 'hidden',
+    height: '100%', // Para que la imagen no se salga de los bordes redondeados
+    flex: 1, // Ocupar todo el espacio vertical disponible
+  },
+  image: {
+    width: '80%', // Ajustar el ancho de la imagen al 100% del contenedor
+    height: 'auto', // Ajustar la altura de la imagen automáticamente
+  },
+  chartContainer: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100%', // El gráfico ocupará toda la altura del contenedor
+    flex: 1, // Ocupar todo el espacio vertical disponible
   },
 }));
 
@@ -56,26 +82,33 @@ export default function ProgressPage() {
           </Typography>
         </Toolbar>
       </AppBar>
-      <Container maxWidth="md">
-        <Grid container spacing={3}>
-          <Grid item xs={12}>
-            <Paper className={classes.paper}>
+
+      <Container maxWidth="xl" style={{ height: '100%' }}>
+        <Grid container spacing={3} style={{ height: '100%', display: 'flex' }}>
+          <Grid item xs={6}>
+            <Paper className={classes.paper} style={{ height: '100%' }}>
               <Typography variant="h4">Tu Progreso</Typography>
               <Typography variant="body1">
                 Aquí puedes ver tu progreso en tareas y hábitos
               </Typography>
+              <div className={classes.chartContainer}>
+                <LineChart width={550} height={550} data={data}>
+                  <Line type="monotone" dataKey="Tareas" stroke="#8884d8" />
+                  <Line type="monotone" dataKey="Hábitos" stroke="#82ca9d" />
+                  <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
+                  <XAxis dataKey="name" />
+                  <YAxis />
+                </LineChart>
+              </div>
             </Paper>
           </Grid>
-          <Grid item xs={12}>
-            <Paper className={classes.paper}>
-              <Typography variant="h5">Progreso de Tareas y Hábitos</Typography>
-              <LineChart width={500} height={300} data={data}>
-                <Line type="monotone" dataKey="Tareas" stroke="#8884d8" />
-                <Line type="monotone" dataKey="Hábitos" stroke="#82ca9d" />
-                <CartesianGrid stroke="#ccc" strokeDasharray="5 5" />
-                <XAxis dataKey="name" />
-                <YAxis />
-              </LineChart>
+          <Grid item xs={6}>
+            <Paper className={classes.paper} style={{ height: '100%' }}>
+              <Typography variant="h4">Mejora tus hábitos</Typography>
+              {/* Mostrar la imagen aquí */}
+              <div className={classes.imageContainer}>
+                <img src={backgroundImage} alt="Mejora tus hábitos" className={classes.image} />
+              </div>
             </Paper>
           </Grid>
         </Grid>
--- Frontend/src/views/TaskListPage.js
+++ Frontend/src/views/TaskListPage.js
@@ -6,19 +6,37 @@ import EditIcon from '@material-ui/icons/Edit';
 import DeleteIcon from '@material-ui/icons/Delete';
 import Checkbox from '@material-ui/core/Checkbox';
 
+import backgroundImage from '../img/sunrise-g96940d752_1280.jpg'; // Ruta de la imagen local
+
 const useStyles = makeStyles((theme) => ({
   root: {
     marginTop: theme.spacing(8),
     display: 'flex',
     flexDirection: 'column',
     alignItems: 'center',
+    backgroundImage: `url(${backgroundImage})`, // Ruta de la imagen de fondo
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+    minHeight: '70vh', // Asegura que el fondo cubra toda la pantalla
+    backgroundColor: '#000000',// Asegura que el fondo cubra toda la pantalla
   },
-  taskList: {
-    marginTop: theme.spacing(2),
+  taskContainer: {
+    maxHeight: '430px',
+    overflowY: 'auto',
+    width: '50%',
+    marginTop: theme.spacing(8),
+    borderRadius: '10px', // Agregar el borde redondeado al contenedor
   },
   paper: {
     padding: theme.spacing(2),
     margin: '10px 0',
+    backgroundColor: '#d1d0e0', // Color celeste pastel (ajusta según tus preferencias)
+  },
+  addButton: {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    margin: theme.spacing(2),
   },
 }));
 
@@ -38,48 +56,80 @@ function TaskListPage() {
       description: 'Descripción de la tarea 2',
       completed: true,
     },
+    {
+      id: 2,
+      title: 'Tarea 2',
+      description: 'Descripción de la tarea 2',
+      completed: true,
+    },
+    {
+      id: 2,
+      title: 'Tarea 2',
+      description: 'Descripción de la tarea 2',
+      completed: true,
+    },
+    {
+      id: 2,
+      title: 'Tarea 2',
+      description: 'Descripción de la tarea 2',
+      completed: true,
+    },
+    {
+      id: 2,
+      title: 'Tarea 2',
+      description: 'Descripción de la tarea 2',
+      completed: true,
+    },
+    {
+      id: 2,
+      title: 'Tarea 2',
+      description: 'Descripción de la tarea 2',
+      completed: true,
+    },
+    
+    // Agrega más tareas aquí si es necesario
   ];
 
   return (
     <Container component="main" maxWidth="md">
       <div className={classes.root}>
-        <Typography component="h1" variant="h5">
+        <Typography component="h1" variant="h7">
           Mis Tareas
         </Typography>
         <Button
           variant="contained"
           color="primary"
           startIcon={<AddIcon />}
-          className={classes.taskList}
+          className={classes.addButton}
         >
           Nueva Tarea
         </Button>
-        {tasks.map((task) => (
-          <Paper key={task.id} className={classes.paper}>
-            <Grid container justify="space-between" alignItems="center">
-              <Grid item>
-                <Checkbox
-                  checked={task.completed}
-                  // Aquí se gestionaría el cambio de estado de la tarea
-                  onChange={() => {}}
-                />
-                <Typography variant="h6">{task.title}</Typography>
-                <Typography variant="body1">{task.description}</Typography>
+        <div className={classes.taskContainer}>
+          {tasks.map((task) => (
+            <Paper key={task.id} className={classes.paper}>
+              <Grid container justify="space-between" alignItems="center">
+                <Grid item>
+                  <Checkbox
+                    checked={task.completed}
+                    onChange={() => {}}
+                  />
+                  <Typography variant="h6">{task.title}</Typography>
+                  <Typography variant="body1">{task.description}</Typography>
+                </Grid>
+                <Grid item>
+                  <IconButton aria-label="edit">
+                    <EditIcon />
+                  </IconButton>
+                  <IconButton aria-label="delete">
+                    <DeleteIcon />
+                  </IconButton>
+                </Grid>
               </Grid>
-              <Grid item>
-                <IconButton aria-label="edit">
-                  <EditIcon />
-                </IconButton>
-                <IconButton aria-label="delete">
-                  <DeleteIcon />
-                </IconButton>
-              </Grid>
-            </Grid>
-          </Paper>
-        ))}
+            </Paper>
+          ))}
+        </div>
       </div>
     </Container>
   );
 }
-
 export default TaskListPage;
