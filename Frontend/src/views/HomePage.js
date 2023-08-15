5d43be075aa6605fc7a8d812569ccf88b758a57c
Nelson Molina <geovannysangucho25@gmail.com>
2023-08-14 22:20:34 -0500

    PaginaPrincipal

--- Frontend/src/views/HomePage.js
+++ Frontend/src/views/HomePage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { AppBar, Toolbar, IconButton, Typography, Button, Container, Grid, Box } from '@material-ui/core';
+import { AppBar, Toolbar, IconButton, Typography, Button, Container, Box } from '@material-ui/core';
 import { Link as RouterLink } from 'react-router-dom';
 import HomeIcon from '@material-ui/icons/Home';
 import ListIcon from '@material-ui/icons/List';
@@ -8,8 +8,8 @@ import TrendingUpIcon from '@material-ui/icons/TrendingUp';
 
 const HomePage = () => {
   return (
-    <div>
-      <AppBar position="sticky">
+    <div style={{ backgroundColor: 'black', color: 'white', display: 'flex', flexDirection: 'column', minHeight: '100vh' }}>
+      <AppBar position="sticky" style={{ backgroundColor: '#00ff40' }}>
         <Toolbar>
           <IconButton edge="start" color="inherit" aria-label="logo">
             <HomeIcon />
@@ -17,46 +17,46 @@ const HomePage = () => {
           <Typography variant="h6" style={{ flexGrow: 1 }}>
             Gestor de Tareas y Hábitos
           </Typography>
+          <Button variant="contained" color="secondary" component={RouterLink} to="/login">
+            Iniciar Sesión
+          </Button>
         </Toolbar>
       </AppBar>
-      <Box bgcolor="primary.main" color="primary.contrastText" py={3}>
-        <Container maxWidth="md">
-          <Typography variant="h2" align="center" gutterBottom>
-            Organiza tus tareas y mejora tus hábitos
-          </Typography>
-          <Typography variant="body1" align="center" paragraph>
-            Nuestro gestor te permite llevar un control de tus tareas y hábitos de una forma fácil y segura. Olvídate de las listas de papel y únete a nuestra plataforma!
-          </Typography>
-          <Box display="flex" justifyContent="center">
-            <Button variant="contained" color="secondary" component={RouterLink} to="/login">Iniciar Sesión</Button>
-          </Box>
-        </Container>
-      </Box>
-      <Container maxWidth="md" py={3}>
-        <Grid container spacing={3}>
-          <Grid item xs={12} sm={4}>
-            <Box display="flex" flexDirection="column" alignItems="center" mb={1}>
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flex: 1 }}>
+        <Box className="caja2" style={{ textAlign: 'center', opacity: 0, transform: 'translateY(20px)', animation: 'appearAnimation 1s forwards' }}>
+          <Container>
+            <Typography
+              variant="h2"
+              gutterBottom
+              className="glowing-text"
+            >
+              Organiza tus tareas y mejora tus hábitos
+            </Typography>
+            <Typography variant="body1" paragraph>
+              Nuestro gestor te permite llevar un control de tus tareas y hábitos de una forma fácil y segura. Olvídate de las listas de papel y únete a nuestra plataforma!
+            </Typography>
+          </Container>
+        </Box>
+        <Box className="caja1" style={{ textAlign: 'center' }}>
+          <Container>
+            <Box className="boxItem" display="flex" flexDirection="column" alignItems="center" mb={1}>
               <ListIcon style={{ fontSize: 60 }} />
               <Typography variant="h6">Gestión de Tareas</Typography>
               <Typography>Administra tus tareas de forma eficiente, sin necesidad de usar papel.</Typography>
             </Box>
-          </Grid>
-          <Grid item xs={12} sm={4}>
-            <Box display="flex" flexDirection="column" alignItems="center" mb={1}>
+            <Box className="boxItem" display="flex" flexDirection="column" alignItems="center" mb={1}>
               <DoneAllIcon style={{ fontSize: 60 }} />
               <Typography variant="h6">Seguimiento de Hábitos</Typography>
               <Typography>Lleva un registro de tus hábitos y ve tu progreso a lo largo del tiempo.</Typography>
             </Box>
-          </Grid>
-          <Grid item xs={12} sm={4}>
-            <Box display="flex" flexDirection="column" alignItems="center" mb={1}>
+            <Box className="boxItem" display="flex" flexDirection="column" alignItems="center" mb={1}>
               <TrendingUpIcon style={{ fontSize: 60 }} />
               <Typography variant="h6">Análisis de Productividad</Typography>
               <Typography>Nuestra aplicación genera análisis para que puedas optimizar tu productividad.</Typography>
             </Box>
-          </Grid>
-        </Grid>
-      </Container>
+          </Container>
+        </Box>
+      </div>
       <Box bgcolor="primary.main" color="primary.contrastText" py={3}>
         <Container maxWidth="md">
           <Typography variant="h4" align="center" gutterBottom>
@@ -67,10 +67,46 @@ const HomePage = () => {
       <Box bgcolor="secondary.main" color="secondary.contrastText" py={3}>
         <Container maxWidth="md">
           <Typography align="center" color="inherit" gutterBottom>
-            © 2023 Mi App | Todos los derechos reservados | 
+            © 2023 Mi App | Todos los derechos reservados |
           </Typography>
         </Container>
       </Box>
+      <style>
+        {`
+          @keyframes appearAnimation {
+            0% { opacity: 0; transform: translateY(20px); }
+            100% { opacity: 1; transform: translateY(0); }
+          }
+          .boxItem {
+            transition: transform 0.3s, opacity 0.3s;
+          }
+          .boxItem:hover {
+            transform: translateY(-10px);
+            opacity: 0.8;
+          }
+
+          .glowing-text {
+            position: relative;
+            display: inline-block;
+            cursor: pointer;
+          }
+
+          .glowing-text:hover {
+            animation: glowAnimation 1.5s infinite;
+          }
+
+          @keyframes glowAnimation {
+            0%, 100% {
+              color: white;
+              text-shadow: none;
+            }
+            50% {
+              color: #00ff40;
+              text-shadow: 0 0 10px rgba(0, 255, 64, 0.8), 0 0 20px rgba(0, 255, 64, 0.8), 0 0 30px rgba(0, 255, 64, 0.8);
+            }
+          }
+        `}
+      </style>
     </div>
   );
 };
--- Frontend/src/views/MainPage.js
+++ Frontend/src/views/MainPage.js
@@ -18,6 +18,7 @@ import { makeStyles } from '@material-ui/core/styles';
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
+    background: '#d1d0e0',
   },
   menuButton: {
     marginRight: theme.spacing(2),
@@ -30,21 +31,37 @@ const useStyles = makeStyles((theme) => ({
     textAlign: 'center',
     color: theme.palette.text.secondary,
   },
+  menu: {
+    marginTop: '50px',
+    width: '200px',
+    background: theme.palette.primary.main,
+  },
+  menuItem: {
+    color: theme.palette.common.white,
+  },
+  pageWrapper: {
+    display: 'flex',
+    flexDirection: 'column',
+    minHeight: '100vh',
+  },
+  footer: {
+    marginTop: 'auto',
+  },
 }));
 
 export default function MainPage() {
   const classes = useStyles();
   const [anchorEl, setAnchorEl] = useState(null);
   const [username, setUsername] = useState('');
-  const navigate = useNavigate();  
+  const navigate = useNavigate();
 
   useEffect(() => {
     const userId = sessionStorage.getItem('userId');
 
     if (userId) {
       fetch(`/usuarios/${userId}`)
-        .then(response => response.json())
-        .then(data => setUsername(data.nombre))
+        .then((response) => response.json())
+        .then((data) => setUsername(data.nombre))
         .catch((error) => console.error('Error:', error));
     }
   }, []);
@@ -60,14 +77,20 @@ export default function MainPage() {
   const logout = () => {
     sessionStorage.removeItem('token');
     sessionStorage.removeItem('userId');
-    navigate('/login'); 
+    navigate('/login');
   };
 
   return (
     <div className={classes.root}>
       <AppBar position="static">
         <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu" onClick={handleClick}>
+          <IconButton
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+            onClick={handleClick}
+          >
             <MenuIcon />
           </IconButton>
           <Menu
@@ -76,44 +99,111 @@ export default function MainPage() {
             keepMounted
             open={Boolean(anchorEl)}
             onClose={handleClose}
+            classes={{ paper: classes.menu }}
           >
-            <MenuItem onClick={() => navigate('/profile')}>Perfil</MenuItem>
-            <MenuItem onClick={() => navigate('/progress')}>Progresos</MenuItem>
+            <MenuItem
+              className={classes.menuItem}
+              onClick={() => navigate('/profile')}
+            >
+              Perfil
+            </MenuItem>
+            <MenuItem
+              className={classes.menuItem}
+              onClick={() => navigate('/progress')}
+            >
+              Progresos
+            </MenuItem>
           </Menu>
           <Typography variant="h6" className={classes.title}>
             Gestor de Hábitos y Tareas
           </Typography>
-          <Button color="inherit" onClick={logout}>Cerrar sesión</Button>
+          <Button color="inherit" onClick={logout}>
+            Cerrar sesión
+          </Button>
         </Toolbar>
       </AppBar>
-      <Container maxWidth="md">
-        <Grid container spacing={3}>
-          <Grid item xs={12}>
-            <Paper className={classes.paper}>
-              <Typography variant="h4">Bienvenido a tu Gestor de Hábitos y Tareas, {username}</Typography>
-              <Typography variant="body1">
-                Organiza tu vida y mejora tus hábitos con nuestro gestor
-              </Typography>
-            </Paper>
-          </Grid>
-          <Grid item xs={6}>
-            <Paper className={classes.paper}>
-              <Typography variant="h5">Tus Tareas</Typography>
-              <Button variant="contained" color="primary" onClick={() => navigate('/task-list')}>
-                Ver Tareas
-              </Button>
-            </Paper>
-          </Grid>
-          <Grid item xs={6}>
-            <Paper className={classes.paper}>
-              <Typography variant="h5">Tus Hábitos</Typography>
-              <Button variant="contained" color="secondary" onClick={() => navigate('/habit-list')}>
-                Ver Hábitos
-              </Button>
-            </Paper>
-          </Grid>
-        </Grid>
-      </Container>
+      <div className={classes.pageWrapper}>
+        <Container class="cont1">
+        <h1>Hola Mndo</h1>
+        </Container>
+        <Container class="cont2">
+          <Container maxWidth="md">
+            <Grid container spacing={2}>
+              <Grid item xs={12}>
+                <Paper className={classes.paper}>
+                  <Typography variant="h4">
+                    Bienvenido a tu Gestor de Hábitos y Tareas, {username}
+                  </Typography>
+                  <Typography variant="body1">
+                    Organiza tu vida y mejora tus hábitos con nuestro gestor
+                  </Typography>
+                </Paper>
+              </Grid>
+              <Grid item xs={6}>
+                <Paper className={classes.paper}>
+                  <Typography variant="h5">Tus Tareas</Typography>
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={() => navigate('/task-list')}
+                  >
+                    Ver Tareas
+                  </Button>
+                </Paper>
+              </Grid>
+              <Grid item xs={6}>
+                <Paper className={classes.paper}>
+                  <Typography variant="h5">Tus Hábitos</Typography>
+                  <Button
+                    variant="contained"
+                    color="secondary"
+                    onClick={() => navigate('/habit-list')}
+                  >
+                    Ver Hábitos
+                  </Button>
+                </Paper>
+              </Grid>
+              <Grid item xs={6}>
+                <Paper className={classes.paper}>
+                  <Typography variant="h5">Tus Hábitos</Typography>
+                  <Button
+                    variant="contained"
+                    color="secondary"
+                    onClick={() => navigate('/habit-list')}
+                  >
+                    Ver Hábitos
+                  </Button>
+                </Paper>
+              </Grid>
+              <Grid item xs={6}>
+                <Paper className={classes.paper}>
+                  <Typography variant="h5">Tus Hábitos</Typography>
+                  <Button
+                    variant="contained"
+                    color="secondary"
+                    onClick={() => navigate('/habit-list')}
+                  >
+                    Ver Hábitos
+                  </Button>
+                </Paper>
+              </Grid>
+            </Grid>
+          </Container>
+        </Container>
+      </div>
+      <footer>
+        <Container
+          className={classes.footer}
+          bgcolor="secondary.main"
+          color="secondary.contrastText"
+        >
+          <Container maxWidth="md">
+            <Typography align="center" color="inherit" gutterBottom>
+              © 2023 Mi App | Todos los derechos reservados |
+            </Typography>
+          </Container>
+        </Container>
+      </footer>
     </div>
   );
 }
